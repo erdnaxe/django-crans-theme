b6568c00645749d3d022b93633f1dd6f913fc09d
Alexandre Iooss <erdnaxe@crans.org>
2019-06-30 17:44:26 +0200

    More testing

--- src/django_crans_theme/tests/test_templates.py
+++ src/django_crans_theme/tests/test_templates.py
@@ -2,10 +2,44 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+"""
+Test that every themed page still works
+"""
 
 
-class TagModelTests(TestCase):
+class TemplateLoggedOutTests(TestCase):
     def test_login_page(self):
         response = self.client.get('/admin/login/')
+        self.assertEqual(response.status_code, 200)
+
+
+class TemplateLoggedInTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username="admin",
+            password="adminadmin",
+            email="admin@example.com",
+        )
+        self.client.force_login(self.user)
+
+    def test_login_page(self):
+        """
+        Login page should redirect
+        """
+        response = self.client.get('/admin/login/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_admin_index(self):
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_accounts_password_reset(self):
+        response = self.client.get('/accounts/password_reset/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_page(self):
+        response = self.client.get('/accounts/logout/')
         self.assertEqual(response.status_code, 200)
 
